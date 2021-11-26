229dc768ff46681ee98e18384e542ea25410460a
hugo <hugof_lacerda@hotmail.com>
2021-11-26 07:14:53 -0800

    ex2 and ex3

--- PL2/3ex.sh
+++ PL2/3ex.sh
@@ -2,17 +2,21 @@
 
 #Number of arguments
 #echo $#
-
 #List each argument
 #echo $@
 
 EXPECTED_N_ARGUMENTS=1
-dirArg = $1
-printf "$dirArg"
-if [[ -d $dirArg ]]
-then
-	printf "Its a dir\n"
-	find $dirArg -type f -name "*.txt"
+#creating dirArg for ease of reading
+dirArg="$1"
+
+if [[ -d "$dirArg" ]]
+	then
+		# used "-maxdepth 1" so that find only searches the given dir by the user, if not, find will go to subdirs 
+		for fileTxt in $(find $dirArg -maxdepth 1 -type f -name "*.txt")
+		do
+			printf "$fileTxt is a txt file\n"
+			head -n 1 "$fileTxt"
+		done
 else
-	printf "Its not a dir\n"
+		printf "$dirArg is not a dir\n"
 fi
