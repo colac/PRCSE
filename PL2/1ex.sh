0d5cf9af04a2505323a39ce4b49f2cb405c4e6de
root <hugof_lacerda@hotmail.com>
2021-11-27 11:51:18 +0000

    altered 1ex.sh

--- PL2/1ex.sh
+++ PL2/1ex.sh
@@ -24,9 +24,8 @@ while test $i -le `expr $number / 2`
 #increment the loop variable
         i=`expr $i + 1`
     done
-if test $f -eq 1 
-    then
-        echo "Not Prime"
-    else
-        echo "Prime"
+if [[ $f -eq 1 ]]; then
+    echo "Not Prime"
+else
+    echo "Prime"
 fi
