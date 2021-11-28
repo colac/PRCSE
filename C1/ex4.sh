ac57d07cbf03d565d4d717a0298140b891e2a3bf
root <hugof_lacerda@hotmail.com>
2021-11-28 00:37:27 +0000

    ex4.sh comments

--- C1/ex4.sh
+++ C1/ex4.sh
@@ -3,25 +3,29 @@
 EXPECTED_N_ARGUMENTS=1
 # Creating dirArg for ease of reading
 dirArg="$1"
-# Declare associative array
-# echo "${arrayFilesLines[@]}" -> gets the values
-# echo "${!arrayFilesLines[@]}" -> gets the keys
+
+# Associative array with all files in the directory and all their number of lines
 declare -A arrayFilesLines
 
-# Array with only 5 elements, the highest number of lines
+# Associative array with only 5 files and their number of lines, the ones that have the highest number of lines
 declare -A arrayTopFiveFiles
+
+# echo "${arrayFilesLines[@]}" -> gets the values, in this case the number of lines of the files in the directory
+# echo "${!arrayFilesLines[@]}" -> gets the keys, in this case the files names
 
 if [[ $# -eq EXPECTED_N_ARGUMENTS ]]; then
     if [[ -d "$dirArg" ]]; then
-        # Array with the lines of all the files 
+        # Array with the lines of all the files in the directory
         arrayTotalNrOfLines=()
-        # Array with only 5 elements, the highest number of lines
+        # Array with the number of lines of the files that have the highest number of lines
         arrayTopFiveLines=()
-
         # Used "-maxdepth 1" so that find only searches the given dir by the user, if not, find will go to subdirs
         for fileTxt in $(find $dirArg -maxdepth 1 -type f -name "*.txt"); do
+            # Gets the number of lines of the $file, https://linuxhint.com/count-the-number-of-lines-in-a-file-in-bash/
             varGetNrOfLines=$(sed -n '$=' "$fileTxt")
+            # Strip directory and suffix from filenames, https://explainshell.com/explain?cmd=basename
             fileTxt=$(basename $fileTxt)
+            # Adding 
             arrayTotalNrOfLines+=("$varGetNrOfLines")
             arrayFilesLines[$fileTxt]=$varGetNrOfLines
         done
@@ -41,6 +45,8 @@ if [[ $# -eq EXPECTED_N_ARGUMENTS ]]; then
                 done
             fi
         done
+        echo "${arrayTopFiveFiles[@]}"
+        echo "${!arrayTopFiveFiles[@]}"
         printf "These ${#arrayFilesLines[@]} directories have the higher line count of directory: "$dirArg"\n"
         for key in "${!arrayTopFiveFiles[@]}"; do echo "File: $key has ${arrayTopFiveFiles[$key]} lines"; done
     else
