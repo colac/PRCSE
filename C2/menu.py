db47da5d8da070fc1a4c0941fbc35a4e58334151
hugo <hugof_lacerda@hotmail.com>
2021-12-05 06:53:16 -0800

    sub-menus created

--- C2/menu.py
+++ C2/menu.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import os
 
-def main():
+# Main menu, the 1st menu the operator sees
+def mainMenu():
     while True:
         #os.system("clear")
         print("\nChoose an option: ")
@@ -15,7 +16,6 @@ def main():
         choice = input("\nEnter your choice : ")
 
         if choice == '1':
-            #aws.aws()
             print(f'\nChoice: {choice}')
         elif choice == '2' :
             print(f'\nChoice: {choice}')
@@ -29,4 +29,105 @@ def main():
         else:
             print(f'\nPlease insert a valid option. Choice: {choice} is NOT valid!')
 
-main()
+
+# User menu, perform operations related to user accounts
+def userMenu():
+    while True:
+        #os.system("clear")
+        print("\nChoose an option: ")
+        print("""
+        1 : Create user account
+        2 : Update/Modify user account
+        3 : Delete user account
+        4 : List information for a user account
+        5 : Return to main menu
+        0 : Exit"""
+              )
+        choice = input("\nEnter your choice : ")
+
+        if choice == '1':
+            print(f'\nChoice: {choice}')
+        elif choice == '2' :
+            print(f'\nChoice: {choice}')
+        elif choice == '3' :
+            print(f'\nChoice: {choice}')
+        elif choice == '4' :
+            print(f'\nChoice: {choice}')
+        elif choice == '5' :
+            print(f'\nChoice: {choice}')
+            mainMenu()
+        elif choice == '0':
+            print(f'\nChoice: {choice}')
+            exit()
+        else:
+            print(f'\nPlease insert a valid option. Choice: {choice} is NOT valid!')
+
+# Resources menu, perform operations related to resources
+def resourcesMenu():
+    while True:
+        #os.system("clear")
+        print("\nChoose an option: ")
+        print("""
+        1 : Create company resource
+        2 : Update/Modify company resource
+        3 : Delete company resource
+        4 : List which users have access to a specific resource
+        5 : List all resources
+        6 : Return to main menu
+        0 : Exit"""
+              )
+        choice = input("\nEnter your choice : ")
+
+        if choice == '1':
+            print(f'\nChoice: {choice}')
+        elif choice == '2' :
+            print(f'\nChoice: {choice}')
+        elif choice == '3' :
+            print(f'\nChoice: {choice}')
+        elif choice == '4' :
+            print(f'\nChoice: {choice}')
+        elif choice == '5' :
+            print(f'\nChoice: {choice}')
+            mainMenu()
+        elif choice == '0':
+            print(f'\nChoice: {choice}')
+            exit()
+        else:
+            print(f'\nPlease insert a valid option. Choice: {choice} is NOT valid!')
+
+# Roles menu, perform operations related to roles
+def rolesMenu():
+    while True:
+        #os.system("clear")
+        print("\nChoose an option: ")
+        print("""
+        1 : Create a role
+        2 : Update/Modify a role
+        3 : Delete role
+        4 : List which users have a specific role
+        5 : List all roles
+        6 : Return to main menu
+        0 : Exit"""
+              )
+        choice = input("\nEnter your choice : ")
+
+        if choice == '1':
+            #aws.aws()
+            print(f'\nChoice: {choice}')
+        elif choice == '2' :
+            print(f'\nChoice: {choice}')
+        elif choice == '3' :
+            print(f'\nChoice: {choice}')
+        elif choice == '4' :
+            print(f'\nChoice: {choice}')
+        elif choice == '5' :
+            print(f'\nChoice: {choice}')
+            mainMenu()
+        elif choice == '0':
+            print(f'\nChoice: {choice}')
+            exit()
+        else:
+            print(f'\nPlease insert a valid option. Choice: {choice} is NOT valid!')
+
+# Calling main menu function
+mainMenu()
