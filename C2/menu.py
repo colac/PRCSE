2dc8fb13cb4572d8fe6209185ce52a9f5ea189a0
hugo <hugof_lacerda@hotmail.com>
2021-12-04 16:10:46 -0800

    first draft of initial menu

---
+++ C2/menu.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+
+def main():
+    while True:
+        #os.system("clear")
+        print("\nChoose an option: ")
+        print("""
+        1 : User accounts actions (leads to another menu with create/delete/modify/list)
+        2 : Company resources actions (leads to another menu with create/delete/modify/list)
+        3 : Roles actions (leads to another menu with create/delete/modify/list)
+        4 : List all information
+        0 : Exit"""
+              )
+        choice = input("\nEnter your choice : ")
+
+        if choice == '1':
+            #aws.aws()
+            print(f'\nChoice: {choice}')
+        elif choice == '2' :
+            print(f'\nChoice: {choice}')
+        elif choice == '3' :
+            print(f'\nChoice: {choice}')
+        elif choice == '4' :
+            print(f'\nChoice: {choice}')
+        elif choice == '0':
+            print(f'\nChoice: {choice}')
+            exit()
+        else:
+            print(f'\nPlease insert a valid option. Choice: {choice} is NOT valid!')
+            #os.system("clear")
+
+main()
